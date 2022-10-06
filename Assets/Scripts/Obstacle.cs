38773861fd3595945a5daf39ae0b916b933e84a0
TAWinterton2 <tawinterton2@alaska.edu>
2022-10-05 17:30:56 -0800

    Updated Obstcale Script
    Redid obstcal script.

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,24 +4,28 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    public   GameObject Thing;
-    public float speed;
+    private Rigidbody2D Wall;
+    [SerializeField] private float speed;
+    [SerializeField] private bool random;
 
-
+   
     // Start is called before the first frame update
     void Start()
     {
-      
+        Wall = this.GetComponent<Rigidbody2D>();
+        Wall.velocity = new Vector2(speed, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(speed * Time.deltaTime, 0, 0);
+        
+       /* transform.Translate(speed * Time.deltaTime, 0, 0);
         if(transform.position.x < -7)
         {
-            Thing.transform.position = new Vector3(7, Thing.transform.position.y, Thing.transform.position.z);
+            Wall.transform.position = new Vector3(7, Wall.transform.position.y, Wall.transform.position.z);
         }
-
+       */
+        
     }
 }
