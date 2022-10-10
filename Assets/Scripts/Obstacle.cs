6051986832c45d04e2e31bb88a3cc167ffe2dcc0
TAWinterton2 <tawinterton2@alaska.edu>
2022-10-10 15:53:32 -0800

    Added Obstacle Despawner

--- Assets/Scripts/Obstacle.cs
+++ Assets/Scripts/Obstacle.cs
@@ -4,28 +4,22 @@ using UnityEngine;
 
 public class Obstacle : MonoBehaviour
 {
-    private Rigidbody2D Wall;
-    [SerializeField] private float speed;
-    [SerializeField] private bool random;
+    public float speed;
+    private Rigidbody2D wall;
+    private Vector2 screenBounds;
 
-   
+
     // Start is called before the first frame update
     void Start()
     {
-        Wall = this.GetComponent<Rigidbody2D>();
-        Wall.velocity = new Vector2(speed, 0);
+        wall = this.GetComponent<Rigidbody2D>();
+        wall.velocity = new Vector2(-speed, 0);
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-       /* transform.Translate(speed * Time.deltaTime, 0, 0);
-        if(transform.position.x < -7)
-        {
-            Wall.transform.position = new Vector3(7, Wall.transform.position.y, Wall.transform.position.z);
-        }
-       */
         
     }
 }
